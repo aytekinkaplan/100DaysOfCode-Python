c1ad090ceb03a543cbe9422678663eb398ff716d
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-07-29 07:52:27 +0300

    Commit

---
+++ Day001/day-001-printing-start.py
@@ -0,0 +1,10 @@
+print("John Doe")
+print("Hello World")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("Yay! Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
+
+print("Hell0" + " " + "World")
