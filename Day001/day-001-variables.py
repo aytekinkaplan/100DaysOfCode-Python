920325c01f07e25fc14844db4e5b5ac868b1f582
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-07-29 09:14:48 +0300

    Commit

---
+++ Day001/day-001-variables.py
@@ -0,0 +1,38 @@
+#Variables
+
+a = 10
+a = 20
+a = 30
+a = 40
+a = 50
+a = 60
+a = 70
+a = 80
+a = 90
+a = 100
+
+print(a)
+
+str = "Hello World"
+print(str)
+
+str = "Hello Again"
+print(str)
+
+str = "I like typing this."
+print(str)
+
+str = "This is fun."
+print(str)
+
+str = "Yay! Printing."
+print(str)
+
+str = "I'd much rather you 'not'."
+print(str)
+
+str = 'I "said" do not touch this.'
+print(str)
+
+
+
