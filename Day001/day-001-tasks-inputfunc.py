a3a0ddfdb7cf095d33a7fb52a272c8242e5cd61e
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-07-29 08:37:30 +0300

    Commit

--- Day001/day-001-tasks-inputfunc.py
+++ Day001/day-001-tasks-inputfunc.py
@@ -1,1 +1,15 @@
 #Input function
+
+name = input("What is your name? ")
+print("Hello " + name)
+
+age = input("How old are you? ")
+print("You are " + age + " years old.")
+
+height = input("How tall are you? ")
+print("You are " + height + " cm tall.")
+
+weight = input("How much do you weigh? ")
+print("You weigh " + weight + " kg.")
+
+
