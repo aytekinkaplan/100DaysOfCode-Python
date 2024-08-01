c59138d53071f465fff53bacdd086fd41d5765b8
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-02 02:21:41 +0300

    Commit

---
+++ Day017/quiz-game-v1/question_model.py
@@ -0,0 +1,9 @@
+class Question:
+    def __init__(self, q_category, q_difficulty, q_question, q_correct_answer, q_incorrect_answers):
+        self.category = q_category
+        self.difficulty = q_difficulty
+        self.question = q_question
+        self.correct_answer = q_correct_answer
+        self.incorrect_answers = q_incorrect_answers
+
+
