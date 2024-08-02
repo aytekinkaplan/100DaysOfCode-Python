926797b5a3e2b470f21b918986826e93e567c8ec
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-03 00:46:10 +0300

    Commit

---
+++ Day018/day-018-importing-modules-installing-packages-and-working-with-aliases.py
@@ -0,0 +1,13 @@
+import turtle as t
+from turtle import Screen
+
+tim = t.Turtle()
+
+for _ in range(15):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
+
+screen = Screen()
+screen.exitonclick()
