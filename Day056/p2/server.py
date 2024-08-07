2d49dd2ac0066b3dbe1396462849102e75e26440
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-08-07 10:17:20 +0300

    Commit

--- Day056/p2/server.py
+++ Day056/p2/server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    random_number = random.randomint(1, 10)
+    random_number = random.randint(1, 10)
     current_year = datetime.datetime.now().year
     return render_template("index.html", num=random_number, year=current_year)
 
@@ -25,6 +25,14 @@ def guess(name):
     requests_data = age_response.json()
     age = requests_data["age"]
     return render_template("guess.html", person_name=name, gender=gender, age=age)
+
+
+@app.route("/blog/<int:num>")
+def get_blog(num):
+    blog_url = "https://api.npoint.io/c790b4d5cab58020d391"
+    response = requests.get(blog_url)
+    all_posts = response.json()
+    return render_template("blog.html", posts=all_posts, num=num)
 
 
 if __name__ == "__main__":
